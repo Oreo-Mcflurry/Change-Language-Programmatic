c99a4b7af307437b90b57738d312e43ef61a969f
유인호 <yuiop1029@naver.com>
2024-12-09 13:25:13 +0900

    init

--- Change Language Programmatic/ContentView.swift
+++ Change Language Programmatic/ContentView.swift
@@ -8,17 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentLanguage = Locale.current.languageCode ?? "en"
+    
+    init() {
+        currentLanguage = UserDefaults.standard.string(forKey: "AppleLanguages") ?? "en"
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Change Language \(currentLanguage)") {
+                currentLanguage = currentLanguage == "en" ? "ko" : "en"
+                UserDefaults.standard.set(["\(currentLanguage)"], forKey: "AppleLanguages")
+                UserDefaults.standard.synchronize()
+            }
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+enum HandicapCases: LocalizedStringKey, CaseIterable {
+    case less10 = "Less than 10"
+    case tenTo25 = "10-25"
+    case more25 = "More than 25"
+    case noHandicap = "I don’t have a handicap"
 }
