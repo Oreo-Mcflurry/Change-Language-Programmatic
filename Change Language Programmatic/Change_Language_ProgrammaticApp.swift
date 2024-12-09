79db8c93268bcba4c5ff33910fa4801c3c9e870c
유인호 <yuiop1029@naver.com>
2024-12-09 09:25:03 +0900

    Initial Commit

---
+++ Change Language Programmatic/Change_Language_ProgrammaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  Change_Language_ProgrammaticApp.swift
+//  Change Language Programmatic
+//
+//  Created by A_Mcflurry on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Change_Language_ProgrammaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
